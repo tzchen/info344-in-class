c467c993eb79792f3e36e7bcc026715dd5df56b0
Tiffany <tzchen@uw.edu>
2017-10-03 15:21:19 -0700

    10-3-17

---
+++ zipsvr/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"runtime"
+	"strings"
+
+	"github.com/info344/info344-in-class/zipsvr/models"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	w.Header().Add("Content-Type", "text/plain")
+
+	fmt.Fprintf(w, "Hello %s!", name)
+}
+
+func memoryHandler(w http.ResponseWriter, r *http.Request) {
+	runtime.GC()
+	stats := &runtime.MemStats{}
+	runtime.ReadMemStats(stats)
+	w.Header().Add("Access-Control-Allow-Origin", "*")
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(stats)
+}
+
+func main() {
+	addr := os.Getenv("ADDR")
+	if len(addr) == 0 {
+		addr = ":80"
+	}
+	zips, err := models.LoadZips("zips.csv")
+	if err != nil {
+		log.Fatalf("error loading zips: %v", err)
+	}
+	log.Printf("loaded %d zips", len(zips))
+
+	cityIndex := models.ZipIndex{}
+	for _, z := range zips {
+		cityLower := strings.ToLower(z.City)
+		cityIndex[cityLower] = append(cityIndex[cityLower], z)
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", helloHandler)
+	mux.HandleFunc("/memory", memoryHandler)
+	fmt.Printf("server is listening at http://%s\n", addr)
+	log.Fatal(http.ListenAndServe(addr, mux))
+}
